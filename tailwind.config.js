d1de25245005172130ae4e0eeb5acd8b6ec6e9e9
Abhijit totager <91001947+abhijittotager@users.noreply.github.com>
2025-03-09 20:01:52 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        paleGreen: "#A8E6CF", // Soft pale green
+      },
+    },
+  },
+  plugins: [],
+};
